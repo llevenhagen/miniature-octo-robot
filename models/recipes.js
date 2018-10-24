4b7f06c68698ee40316aef98117d40c3b57065cb
Brooke <Brooke@Mac.local>
2018-10-24 11:51:39 -0400

    changed model

--- models/recipes.js
+++ models/recipes.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const recipeSchema = new mongoose.Schema({
     name: String,
     time: String,
-    ingredients: [String]
+    ingredients: [String],
+    img: String
 })
 
 const Recipes = mongoose.model('Recipe', recipieSchema)
