a4d5afe2d99fffcb7005cec5e2a8bf446e57e25f
Cyndy Koobs <cyndy.koobs@att.net>
2018-10-24 17:26:43 -0500

    Can CREATE recipes, logic working for no details, edit

--- public/js/app.js
+++ public/js/app.js
@@ -4,36 +4,62 @@ app.controller('groupController', ['$http', function($http) {
   // this.foo='bar';
 
   const controller = this;
-    //
-    // this.createRecipe = function() {
-    //   $http({
-    //     method: 'POST',
-    //     url: '/recipes',
-    //     data: {
-    //       name: this.name,
-    //       time:  this.time,
-    //       img: this.img,
-    //       ingredients: this.ingredients
-    //     }
-    //   }).then(function(response) {
-    //     console.log(response);
-    //     controller.recipes();
-    //   }, function() {
-    //     console.log('error');
-    //   });
-    // }
-    this.getRecipes = () => {
-      $http({
-        method: 'GET',
-        url: '/recipes'
-      }).then((res) => {
-        console.log(res.data);
-        controller.allrecipes = res.data;
-      }, (err) => {
-        console.log('error');
-      });
-    }
+  // ==================================
+  //          VARIABLES
+  // ==================================
+  // default variables
+  this.addRecipe = false;
+  this.detailsShow = false;
+
+  this.getRecipes = () => {
+    $http({
+      method: 'GET',
+      url: '/recipes'
+    }).then((res) => {
+      console.log(res.data);
+      controller.allrecipes = res.data;
+    }, (err) => {
+      console.log('error');
+    });
+  }
+  // this.createRecipe() = () => {
+  //     $http({
+  //       method: 'POST',
+  //       url: '/recipes',
+  //       data: {
+  //         name: this.name,
+  //         time:  this.time,
+  //         img: this.img,
+  //         ingredients: this.ingredients
+  //       }
+  //     }).then((res) {
+  //       this.getRecipes();
+  //     }, (err) => {
+  //       console.log('error');
+  //     });
+  //   }
+    this.createRecipe = function() {
+       $http({
+         method: 'POST',
+         url: '/recipes',
+         data: {
+           name: this.name,
+           time:  this.time,
+           img: this.img,
+           ingredients: this.ingredients
+         }
+       }).then(function(response) {
+         console.log(response);
+         controller.getRecipes();
+       }, function() {
+         console.log('error');
+       });
+     }
+    // this.updateRecipe = (id)
     // call on page load
     this.getRecipes();
 
+    this.toggleShowAddForm = () => {
+      this.addRecipe = !this.addRecipe;
+    }
 }]);  // end app.controller
