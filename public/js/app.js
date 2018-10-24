7650e64b475fdcae1a88e7813b94402db3601129
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2018-10-24 15:26:43 -0700

    because I'm about to pull

--- public/js/app.js
+++ public/js/app.js
@@ -4,24 +4,24 @@ app.controller('groupController', ['$http', function($http) {
   // this.foo='bar';
 
   const controller = this;
-    //
-    // this.createRecipe = function() {
-    //   $http({
-    //     method: 'POST',
-    //     url: '/recipes',
-    //     data: {
-    //       name: this.name,
-    //       time:  this.time,
-    //       img: this.img,
-    //       ingredients: this.ingredients
-    //     }
-    //   }).then(function(response) {
-    //     console.log(response);
-    //     controller.recipes();
-    //   }, function() {
-    //     console.log('error');
-    //   });
-    // }
+    
+    this.createRecipe = function() {
+      $http({
+        method: 'POST',
+        url: '/recipes',
+        data: {
+          name: this.name,
+          time:  this.time,
+          img: this.img,
+          ingredients: this.ingredients
+        }
+      }).then(function(response) {
+        console.log(response);
+        controller.recipes();
+      }, function() {
+        console.log('error');
+      });
+    }
     this.getRecipes = () => {
       $http({
         method: 'GET',
